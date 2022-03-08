0baa7e68270ae8c0a33161d4f1fd9a32035305ab
ario <cybera.3s@gmail.com>
2022-03-09 02:39:44 +0330

    register OtpCodeAdmin

--- train_ecommerce/accounts/admin.py
+++ train_ecommerce/accounts/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserADmin
 from django.contrib.auth.models import Group
 
-from .models import User
+from .models import User, OtpCode
 from .forms import UserCreationForm, UserChangeForm
+
+
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'code', 'created')
 
 
 class UserAdmin(BaseUserADmin):
