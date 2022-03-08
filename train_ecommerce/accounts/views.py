219c9f22cd7e299fd8417f0e476c8fb779e19e01
ario <cybera.3s@gmail.com>
2022-03-09 02:41:03 +0330

    create UserRegisterView

--- train_ecommerce/accounts/views.py
+++ train_ecommerce/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from .forms import *
+
+
+class UserRegisterView(View):
+    form_class = UserRegisterationForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'accounts/register.html', {'form':form})
+
+    def post(self, request):
+        ...
