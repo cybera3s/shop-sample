feedbd32754aab35d1a46b80ccebd58dba09c9f2
ario <cybera.3s@gmail.com>
2022-03-09 03:23:07 +0330

    refactor UserRegisterView

--- train_ecommerce/accounts/views.py
+++ train_ecommerce/accounts/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+import random
+from utils import send_otp_code
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
 from .forms import *
+from .models import *
 
 
 class UserRegisterView(View):
-    form_class = UserRegisterationForm
+    form_class = UserRegistrationForm
 
     def get(self, request):
         form = self.form_class
-        return render(request, 'accounts/register.html', {'form':form})
+        return render(request, 'accounts/register.html', {'form': form})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -20,7 +23,7 @@ class UserRegisterView(View):
             send_otp_code(cd['phone'], random_code)
 
             OtpCode.objects.create(phone_number=cd['phone'], code=random_code)
-            request.session['user_registeration_info'] = {
+            request.session['user_registration_info'] = {
                 'phone_number': cd['phone'],
                 'email': cd['email'],
                 'full_name': cd['full_name'],
@@ -32,8 +35,27 @@ class UserRegisterView(View):
 
 
 class UserRegisterVerifyCodeView(View):
+    form_class = VerifyCodeForm
+
     def get(self, request):
-        ...
+        form = self.form_class
+        return render(request, 'accounts/verify_code.html', {'form': form})
 
     def post(self, request):
-        ...
+        user_session = request.session['user_registration_info']
+        code_instance = OtmCode.objects.get(phone_number=user_session['phone'])
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+
+            if cd['code'] == code_instance.code:
+                user = User.objects.create_user(user_session['phone_number'], user_session['email'],
+                                                user_session['full_name'], user_session['password'])
+                code_instance.delete()
+                messages.success(request, 'registered', 'success')
+            else:
+                messages.error(request, 'wrong code!', 'danger')
+                return redirect('accounts:verify_code')
+
+        return redirect('home:home')
