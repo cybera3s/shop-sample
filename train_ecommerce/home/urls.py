f852c1b4724a009853607e5df28a9ecc89700f4d
ario <cybera.3s@gmail.com>
2022-03-09 23:12:42 +0330

    add category_filter url route

--- train_ecommerce/home/urls.py
+++ train_ecommerce/home/urls.py
@@ -7,4 +7,5 @@ app_name = 'home'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:category_slug>', HomeView.as_view(), name='category_filter'),
 ]
