c1d7d42c67a1e0a3303fde411d9c170a279adb20
ario <cybera.3s@gmail.com>
2022-03-09 23:34:55 +0330

    add CartAddView

--- train_ecommerce/orders/views.py
+++ train_ecommerce/orders/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+
+from home.models import Product
+from orders.cart import Cart
+from orders.forms import CartAddForm
 
 
 class CartView(View):
@@ -8,3 +12,13 @@ class CartView(View):
 
     def post(self):
         ...
+
+
+class CartAddView(View):
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
