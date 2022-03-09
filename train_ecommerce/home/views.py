fcfe09e154d2f1fbe52059841a4b106167a41b74
ario <cybera.3s@gmail.com>
2022-03-09 23:13:17 +0330

    add category filter > HomeView

--- train_ecommerce/home/views.py
+++ train_ecommerce/home/views.py
@@ -5,15 +5,20 @@ from django.views import View
 
 
 class HomeView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'home/home.html', {'products': products})
+        categories = Category.objects.all()
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'home/home.html', {'products': products, 'categories': categories})
 
 
 class ProductDetailView(View):
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
-        return render(request, 'home/detail.html', {'product': product})
+        categories = Category.objects.all()
+        return render(request, 'home/detail.html', {'product': product, 'categories': categories})
 
     def post(self, request):
         ...
