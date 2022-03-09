f3f862b6f61bdc6f60a2cc20d0452b4d6b5481a1
ario <cybera.3s@gmail.com>
2022-03-09 14:37:45 +0330

    add is_active property > OtpCode

--- train_ecommerce/accounts/models.py
+++ train_ecommerce/accounts/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from .managers import UserManager
@@ -33,6 +35,10 @@ class OtpCode(models.Model):
     phone_number = models.CharField(max_length=11)
     code = models.PositiveSmallIntegerField()
     created = models.DateTimeField(auto_now=True)
+
+    @property
+    def is_active(self):
+        return (self.created + timedelta(minutes=2)) > timezone.now()
 
     def __str__(self):
         return f'{self.phone_number} - {self.code} - {self.created}'
