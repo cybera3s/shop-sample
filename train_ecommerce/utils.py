b5cd3ff14084daf999b03079771fc6be86ada740
ario <cybera.3s@gmail.com>
2022-03-09 13:48:56 +0330

    create utils.py

--- train_ecommerce/utils.py
+++ train_ecommerce/utils.py
@@ -1,2 +1,17 @@
+from kavenegar import *
+
+
 def send_otp_code(phone_number, code):
-    ...
+    try:
+
+        api = KavenegarAPI('4A46746644386C69505A736C6C4959522F66587A427A78633052744B3373694345785A4D49503772352F6F3D')
+        msg = f'your verification code is \n {code}'
+        params = {'sender': '', 'receptor': phone_number, 'message': msg}
+
+        response = api.sms_send(params)
+        print(response)
+
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)
