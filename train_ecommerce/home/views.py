a08ac24350cd4fd567fb5c5af71e98f8cc7bf7dd
ario <cybera.3s@gmail.com>
2022-03-09 15:09:13 +0330

    add ProductDetailView

--- train_ecommerce/home/views.py
+++ train_ecommerce/home/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-
-# Create your views here.
+from .models import *
 from django.views import View
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        products = Product.objects.filter(available=True)
+        return render(request, 'home/home.html', {'products': products})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail.html', {'product': product})
+
+    def post(self, request):
+        ...
