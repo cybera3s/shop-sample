5c6d7181236cf8cd277e242c4cd29245b07a37d9
ario <cybera.3s@gmail.com>
2022-05-09 16:50:07 +0430

    add bucket url

--- train_ecommerce/home/urls.py
+++ train_ecommerce/home/urls.py
@@ -6,6 +6,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('bucket/', BucketHome.as_view(), name='bucket'),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<slug:category_slug>', HomeView.as_view(), name='category_filter'),
 ]
