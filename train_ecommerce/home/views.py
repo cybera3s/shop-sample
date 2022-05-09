567174cd715556fc65d8c947995d00b4c02f3c41
ario <cybera.3s@gmail.com>
2022-05-09 16:50:37 +0430

    add BucketHome view

--- train_ecommerce/home/views.py
+++ train_ecommerce/home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-
-from orders.forms import CartAddForm
+from orders.forms import *
 from .models import *
 from django.views import View
+from .tasks import all_bucket_objects_task
 
 
 class HomeView(View):
@@ -23,3 +23,18 @@ class ProductDetailView(View):
 
     def post(self, request):
         ...
+
+
+class BucketHome(View):
+    template_name = 'home/bucket.html'
+
+    def get(self, request):
+        objects = all_bucket_objects_task()
+
+        return render(request, self.template_name, {'objects': objects})
+
+
+# class DeleteBucket(view):
+#     def get(self, request, obj_name):
+#         delete_bucket_object_task(obj_name)
+#         return redirect('home:bucket')
