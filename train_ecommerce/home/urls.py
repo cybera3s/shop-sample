175879ac0cbc23786300485bfa515d80ffadb3d9
ario <cybera.3s@gmail.com>
2022-05-09 17:54:41 +0430

    add bucket_urls

--- train_ecommerce/home/urls.py
+++ train_ecommerce/home/urls.py
@@ -1,12 +1,18 @@
 
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 app_name = 'home'
 
+bucket_urls = [
+    path('', BucketHome.as_view(), name='bucket'),
+    path('delete_obj_bucket/<key>', DeleteBucketObject.as_view(), name='delete_obj_bucket'),
+]
+
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('bucket/', BucketHome.as_view(), name='bucket'),
+    path('bucket/', include(bucket_urls)),
     path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<slug:category_slug>', HomeView.as_view(), name='category_filter'),
 ]
